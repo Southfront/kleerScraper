bf6380311c014af184911bd2b3e377400597112d
Fredrik Söderström <fredrik.soderstrom@forefront.se>
2025-04-25 09:27:24 +0200

    remove unused vars

--- kleerOneStep.js
+++ kleerOneStep.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 import querystring from "querystring";
 import fs from "fs/promises";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /**
  * Loggar in på Kleer och returnerar autentiseringstoken
